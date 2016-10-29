4b5c52abb7fbed94c5cb8757c8925582682ad92e
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2016-10-29 14:20:58 -0700

    Added Basic Input Validation, and Finished Design
    Added Basic Input Validation, and Finished Design. A new JS file
    validates input and displays error messages if the user forgot to input
    a name or enter a diffiiculty level.

---
+++ Math Learning Game Web Application/js/gameLogic.js
@@ -0,0 +1,83 @@
+/* Get Dynamic Elements */
+
+//Holds the value of the input box where the player types in his name
+var playerName = document.getElementById("nameInput").value;
+
+//Get the the option chosen for the diffulty level
+//var difficultyLevel = document.querySelector("input[name='level-options']:checked").value;
+
+/* Get the type of math buttons */
+var mathTypeButton = document.getElementsByClassName("mathType-button");
+var additionButton = document.getElementById("addition-Button");
+var subtractionButton = document.getElementById("subtraction-Button");
+var multiplicationButton = document.getElementById("multiplication-Button");
+var divisionButton = document.getElementById("division-Button");
+
+/* Call the initializeGame() method when any of the math-Type buttons are clicked. 
+	This code has to be utilized for the mathTypeButton variable returns a NodeList 
+	object which does not work in an eventListener. An eventListener requires 
+	a Node, and not a NodeList which the document.getElementsByClassName returns.
+	This is a workaround for this, converting the NodeList to a Node. 
+
+		Syntax: [].forEach.call(nodeList,function(e){e.addEventListener('click',callback,false)})
+		Source: https://stackoverflow.com/questions/12362256/addeventlistener-on-nodelist
+*/
+[].forEach.call(mathTypeButton, function(e) {
+							  	e.addEventListener('click',initializeGame,false)
+							  } //end function(e)
+				);
+
+//mathTypeButton.addEventListener("click", initializeGame);
+
+function initializeGame() {
+	console.log("A Math Type Button Was Clicked.");
+    validateInputs(); 
+} //end initializeGame() button
+
+/* Check to see if the player name input box is empty and/or
+   a difficulty level hasn't been selected. Returns an error
+   prompting the user to do select the said options.  */
+function validateInputs() {
+	if (playerName === "" || playerName === null || 
+		playerName.length == 0 || isEmpty(playerName) == true ) {
+		console.log("Please enter a Name");
+		document.getElementById("nameInput-errorMessage").style.display = "inline-block";
+	} //end if statement
+
+	if (checkDifficultyLevel() == false) {
+		console.log("Please choose a difficulty level");
+		document.getElementById("difficultyLevel-errorMessage").style.display = "inline-block";
+	} //end if-statement
+
+	if (checkDifficultyLevel() == true) {
+		document.getElementById("difficultyLevel-errorMessage").style.display = "none";
+	} //end if-statement
+
+} //end checkInputs() function
+
+/* Checks to see if there is whitespace in an element
+   like an input field. Returns true if the field is empty.
+*/
+function isEmpty(str){
+    return !str.replace(/^\s+/g, '').length; // boolean (`true` if field is empty)
+}
+
+function checkDifficultyLevel() {
+	var diffLevelRadioButton = document.forms["difficutyLevelForm"]["level-options"];
+	var diffLevelFormLength = diffLevelRadioButton.length;
+	var levelChosen = null;
+
+	for (var i = 0; i < diffLevelFormLength; i++) {
+		if (diffLevelRadioButton[i].checked) {
+			levelChosen = diffLevelRadioButton[i].value;
+		} //end if-statement
+	} //end for loop
+
+	if (levelChosen == null) {
+		return false;
+	} //end if-statement
+
+	else {
+		return true;
+	} //end else-statement
+} //end checkDifficultyLevel() option
