b2f778c0ef9f65d607e6ef9ae5d9d9074773faf4
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2016-11-21 18:17:54 -0800

    Fixed Small Design Issues On Mobile
    Fixed the issue of the modal not scrolling, along with other issues such
    as the equation text breaking into two columns on mobile

--- Math Learning Game Web Application/js/gameLogic.js
+++ Math Learning Game Web Application/js/gameLogic.js
@@ -1,26 +1,34 @@
 
 if (window.location.pathname == "/gameRun.html") {
 	window.onload = function () {
-		console.log(user);
     	timer();
     	ajaxCall();
     	document.getElementById("levelNumber").textContent = difficultyLevelNumber;
     	getMathTypeButton(); 
-    	console.log("User.difficultyLevel: " + user.difficultyLevel);
-    	console.log("Type of math selected: " + mathType);
-	} //end onload inline function
-}  //end if-statement
+	} 
+} 
 
+var userArray = [];
 var userInfo = localStorage.getItem("userInfo");
 var user = JSON.parse(userInfo);	
 var difficultyLevelNumber = user.difficultyLevel;	
 var mathType = user.mathType;
+var username = user.userName;
 
 document.getElementById("submitAnswer-Button").addEventListener("click", function() {answerResult(num1,num2,mathType,userAnswer.value)});
-var timeLeft = 30;
+document.getElementById("game-end-main-menu-button").addEventListener("click", function() {window.location.href = "/index.html";});
+document.getElementById("main-Menu-Button").addEventListener("click", function() {window.location.href = "/index.html";});
+document.getElementById("submit-Score-Button").addEventListener("click", function() {
+     storeHighScore();
+     window.location.href = "/index.html";
+     });
+
+var diff = parseInt(difficultyLevelNumber);
+var goalPoints = (15 * diff) + 15;
+document.getElementById("goalPoints").innerHTML = goalPoints.toString();
+var timeLeft = (40 + (diff * 15));
 var secondsElapsed = 0;
-var counter = setInterval(timer, 1000); 
-//var userAnswer = document.getElementById("answerInputInputBox");
+var counter = setInterval(timer, 100); 
 var num1;
 var num2; 
 var userAnswer= document.getElementById("answerInputInputBox");
@@ -31,32 +39,84 @@ var currentPoints = 0;
 function getMathTypeButton() {
 	if (mathType == "+") {
 		document.getElementById("addition-Button2").style.display = "inline-block";
-	} //end if-statement
+	} 
 
 	if (mathType == "-") {
 		document.getElementById("subtraction-Button2").style.display = "inline-block";
-	} //end if-statement
+	} 
 
 	if (mathType == "*") {
 		document.getElementById("multiplication-Button2").style.display = "inline-block";
-	} //end if-statement
+	} 
 
 	if (mathType == "/") {
 		document.getElementById("division-Button2").style.display = "inline-block";
-	} //end if-statement
-} //end getMathTypeButton()
-
+	} 
+}
+function storeHighScore(){
+    user.time = secondsElapsed.toString();
+	user.score = (Math.round(weightScore()*100)/100).toString();
+    var gameInfo;
+	var getLocal = localStorage.getItem("highscoresList");
+    if (getLocal == null){
+			userArray.push(user);
+          gameInfo = JSON.stringify(userArray);
+    } else{
+		//localStorage.clear();
+		var scoreList = JSON.parse(getLocal);
+		scoreList.push(user);
+		userArray = scoreList;
+		gameInfo = JSON.stringify(userArray);
+		
+      //gameInfo = localStorage.getItem("highscoresList") + "," + username + " " + difficultyLevelNumber + " " + mathType + " " + user.time + " " + (Math.round(weightScore()*100)/100).toString();   
+    }
+    localStorage.setItem("highscoresList", gameInfo);
+}
+function getNumberMathType(type){
+    if (type == "+") { return 1}
+	else if (type == "-") { return .75;}
+	else if (type == "*") { return .5; }
+	else { return .25; }
+}
+function weightScore(){
+    return getNumberMathType(mathType) * secondsElapsed + parseInt(difficultyLevelNumber)*2;
+}
 function timer() {
-  timeLeft--;
-  secondsElapsed++;
+  timeLeft -= 0.1;
+  timeLeft = Math.round(timeLeft * 100) / 100;
+  secondsElapsed += .1;
+  secondsElapsed = Math.round(secondsElapsed * 100) / 100;
   if (timeLeft < 0) {
+	  secondsElapsed -= .1;
+      secondsElapsed = Math.round(secondsElapsed * 100) / 100;
+	  gameEnd(false);
      clearInterval(counter);
      return;
   } 
 
- document.getElementById("secondsLeft").textContent = timeLeft + " secs" + "\n" + "secondsElasped: " + secondsElapsed;
+ document.getElementById("secondsLeft").textContent = timeLeft.toString();
 } 
+function convertMathType(type){
+	if (type == "+") { return "Addition";}
+	else if (type == "-") { return "Subtraction";}
+	else if (type == "*") { return "Multiplication"; }
+	else { return "Division"; }
+}
 
+	
+function gameEnd(winOrLose) {
+	if(winOrLose) {
+		document.getElementById("endMsg").innerHTML = "You Won!";
+	}
+	else {
+		document.getElementById("endMsg").innerHTML = "You Lost!";
+	}
+	document.getElementById("user-name").innerHTML = username.length > 9 ? username.substring(0,8)+"..." : username;
+	document.getElementById("user-lvl").innerHTML = difficultyLevelNumber;
+	document.getElementById("user-type").innerHTML = convertMathType(mathType);
+	document.getElementById("user-time").innerHTML = secondsElapsed.toString();
+	window.location.href = "/gameRun.html#openModal";
+}
 
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : event.keyCode
@@ -72,109 +132,125 @@ function generateTwoRandomNumbers(difficultyLevel) {
 
 	switch (difficultyLevel) {
 		case "1":
-			number1 = Math.floor((Math.random() * 10 ) );
-			number2 = Math.floor((Math.random() * 9 ) + 1 );
-			while (number1 < number2) {
-				number1 = Math.floor((Math.random() * 10 ) );
-				number2 = Math.floor((Math.random() * 9 ) + 1 );
+			number1 = Math.floor((Math.random() * 21 ) );
+			number2 = Math.floor((Math.random() * 19 ) + 1 );
+			while ((number1 < number2) || (number1%number2 != 0)) {
+				number1 = Math.floor((Math.random() * 21 ) );
+				number2 = Math.floor((Math.random() * 19 ) + 1 );
 			}  
 			break;
 
 		case "2":
-			number1 = Math.floor((Math.random() * 90 ) + 10 );
-			number2 = Math.floor((Math.random() * 9 ) + 1 );
+			number1 = Math.floor((Math.random() * 80 ) + 20 );
+			number2 = Math.floor((Math.random() * 19 ) + 1 );
+            while(number1 % number2 != 0){
+                number1 = Math.floor((Math.random() * 80 ) + 20 );
+		      	number2 = Math.floor((Math.random() * 19 ) + 1 );
+            }
 			break;	
 
 		case "3":
-			number1 = Math.floor((Math.random() * 90 ) + 10 );
-			number2 = Math.floor((Math.random() * 90 ) + 10 );
-			while (number1 < number2) {
-				number1 = Math.floor((Math.random() * 90 ) + 10 );
-				number2 = Math.floor((Math.random() * 90 ) + 10 );
+			number1 = Math.floor((Math.random() * 290 ) + 10 );
+			number2 = Math.floor((Math.random() * 49 ) + 1 );
+			while ((number1 < number2) || (number1 % number2 != 0)) {
+				number1 = Math.floor((Math.random() * 290 ) + 10 );
+				number2 = Math.floor((Math.random() * 49 ) + 1 );
 			} 
 			break;
 
 		case "4":
-			number1 = Math.floor((Math.random() * 900 ) + 100 );
-			number2 = Math.floor((Math.random() * 9 ) + 1 );
+			number1 = Math.floor((Math.random() * 490 ) + 10 );
+			number2 = Math.floor((Math.random() * 69 ) + 20 );
+            while(number1 % number2 != 0){
+                number1 = Math.floor((Math.random() * 490 ) + 10 );
+		      	number2 = Math.floor((Math.random() * 69 ) + 20 );
+            }
 			break;
 
 		case "5":
 			number1 = Math.floor((Math.random() * 900 ) + 100 );
-			number2 = Math.floor((Math.random() * 90) + 10 );
+			number2 = Math.floor((Math.random() * 49) + 1 );
+            while (number1 % number2 != 0){
+                number1 = Math.floor((Math.random() * 900 ) + 100 );
+		      	number2 = Math.floor((Math.random() * 49) + 1 );
+            }
 			break;	
 
 		case "6":
 			number1 = Math.floor((Math.random() * 900 ) + 100 );
-			number2 = Math.floor((Math.random() * 900 ) + 100 );
-			while(number1 < number2) {
+			number2 = Math.floor((Math.random() * 500 ) + 20 );
+			while((number1 < number2) || (number1 % number2 != 0)) {
 				number1 = Math.floor((Math.random() * 900 ) + 100 );
-				number1 = Math.floor((Math.random() * 900 ) + 100 );
+				number2 = Math.floor((Math.random() * 500 ) + 20 );
 			}  
 			break;
-
-		/*default:
-			number1 = 3;
-			number2 = 1; */
 	} 
 	generatedNumbers[0] = number1;
 	generatedNumbers[1] = number2;
     num1 = number1;
     num2 = number2;
-    //userAnswer = num1+num2;
 	return generatedNumbers;
 } 
 
 function userAnswerChecker(number1, number2, mathType, userAnswer) {
-    console.log("This is your answer: " + userAnswer);
-    console.log("This is the first and second number: " + number1 + " " + number2);
-    
 	var correctAnswer;
     var isCorrect;
 	switch (mathType) {
 		case "+":
 			correctAnswer = number1 + number2;
-			console.log("Correct Answer: " + correctAnswer + "\n");
 			isCorrect = userAnswer == correctAnswer;
 			break;
 		case "-":
 			correctAnswer = number1 - number2;
-			console.log("Correct Answer: " + correctAnswer + "\n");
 			isCorrect = userAnswer == correctAnswer;
 			break;
 		case "*":
 			correctAnswer = number1 * number2;
-			console.log("Correct Answer: " + correctAnswer + "\n");
 			isCorrect = userAnswer == correctAnswer;
 			break;
 		case "/":
-			correctAnswer = Math.round(number1 / number2);
-			console.log("Correct Answer: " + correctAnswer + "\n");
+			correctAnswer = number1 / number2;
 			isCorrect = userAnswer == correctAnswer;
 			break;
 	} 
+	displayCorrectnessImg(isCorrect);	//Display the "check" or "x" img if correct or not
     return isCorrect;
 } 
 
 function answerResult(number1, number2, mathType, userAnswer) {
-    //console.log(userAnswerChecker(number1,number2,mathType,userAnswer) ? "Correct" : "Wrong");
-    if ( userAnswerChecker(number1,number2,mathType,userAnswer) == true ) {
-    	console.log("Correct answer! :)");
-    	currentPoints++;
-    	currentPointsDOMElement.textContent = currentPoints;
-    	timeLeft += 8;
+    if (userAnswerChecker(number1,number2,mathType,userAnswer)) {
+        difficultyLevelNumber = parseInt(difficultyLevelNumber);
+    	currentPoints += (5 * difficultyLevelNumber + (difficultyLevelNumber + 2));
+        difficultyLevelNumber = difficultyLevelNumber.toString();
+    	currentPointsDOMElement.textContent = currentPoints.toString();
+		if (currentPoints >= goalPoints) {
+			clearInterval(counter);	
+			secondsElapsed -= .1;
+            secondsElapsed = Math.round(secondsElapsed * 100) / 100;
+			gameEnd(true);
+		}
      } 
  	else {
- 		console.log("Incorrect answer :(" + "\n");
- 		currentPoints--;
+ 		currentPoints -= 4 * difficultyLevelNumber;
  		if (currentPoints <= 0 ) {
  			currentPoints = 0;
- 		} //end if-statement
- 		currentPointsDOMElement.textContent = currentPoints;
- 		timeLeft -= 3;
+ 		} 
+ 		currentPointsDOMElement.textContent = currentPoints.toString();
  	}      
     ajaxCall();
-} //end answerResult(number1, number2, mathType, userAnswer) method.
+} 
+
+function displayCorrectnessImg(TorF) {
+	var checkImg;
+	if(TorF) {
+		checkImg = document.getElementById("checkMarkWin");
+	}
+	else {
+		checkImg = document.getElementById("checkXLose");
+	}
+		fadeIn(checkImg, 700);
+		setTimeout(function () {fadeOut(checkImg, 700);}, 800);
+}
 
 function getEquation(twoNumbers) {
 	var currentEquation = twoNumbers[0] + " " +  getMathType() + " " + twoNumbers[1] + "    = ";
@@ -182,25 +258,72 @@ function getEquation(twoNumbers) {
 } 
 
 function runActualGame() {
-	console.log("Running actual game function. Clears answer input when changing equation" + "\n" + "\n");
 	if (userAnswer.value != "") {
 		userAnswer.value = "";
-	} //end if-statement
-} //end runActualGame() function
+	} 
+}
 
 function getMathType() {
 	if (mathType == "*") {
-		return "*";
-	} //end if-statement
+		return "x";
+	}
 
 	else if (mathType == "/") {
-		return "/";
-	} //end if-statement
+		return"\xF7";
+	} 
 
 	else {
 		return mathType;
-	} //end else-statement
-} //end getMathType()
+	} 
+} 
+
+function fadeIn(el, time) {
+     el.style.opacity = 0;
+	 el.style.visibility = "visible";
+     el.style.display = "block";
+
+     var last = +new Date();
+     var tick = function() {
+          el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+          last = +new Date();
+
+          if (+el.style.opacity < 1) {
+               (window.requestAnimationFrame && requestAnimationFrame(tick)) ||      setTimeout(tick, 16)
+          }
+     };
+
+     tick();
+}
+
+function fadeOut( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  if( ms )
+  {
+    var opacity = 1;
+    var timer = setInterval( function() {
+      opacity -= 50 / ms;
+      if( opacity <= 0 )
+      {
+        clearInterval(timer);
+        opacity = 0;
+        elem.style.display = "none";
+        elem.style.visibility = "hidden";
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 0;
+    elem.style.filter = "alpha(opacity=0)";
+    elem.style.display = "none";
+    elem.style.visibility = "hidden";
+  }
+}
 
 function ajaxCall() {
     var request;
@@ -214,8 +337,8 @@ function ajaxCall() {
         if ((request.readyState === 4) && (request.status === 200)) {
             document.getElementById("generatedQuestion").textContent = getEquation(generateTwoRandomNumbers(difficultyLevelNumber));
             runActualGame();
-        } //end if-statement
-    } //end request.onreadystatechange inline function
+        } 
+    } 
     request.send();
-} //end ajaxCall() function
+} 
 
--- Math Learning Game Web Application/js/menuValidation.js
+++ Math Learning Game Web Application/js/menuValidation.js
@@ -2,30 +2,20 @@
 
 //Holds the input box element where the player types in his name
 var playerName = document.getElementById("nameInput");
+var userArray = localStorage.getItem("highscoresList");
+var objArray = JSON.parse(userArray);
 
-//Get the the option chosen for the diffulty level
 var difficultyLevelRadioButton = document.forms["difficultyLevelForm"]["level-options"];
-//document.querySelector("input[name='level-options']");
 var difficultyLevelRadioButton_Checked = document.querySelector('input[name="level-options"]:checked');
-//difficultyLevelRadioButtonChecked();
 
-//document.querySelector("input[name='level-options']:checked"
 
 function difficultyLevelRadioButtonChecked() {
 	var difficultyLevelRadioButtonOption;
-	/*if (checkDifficultyLevel() == false) {
-		difficultyLevelRadioButtonOption = document.forms["difficultyLevelForm"]["level-options"][Math.floor(Math.random() * 4 )];
-	} //end if-statement*/
-
-	/*else if (checkDifficultyLevel() == true) { */
 		difficultyLevelRadioButtonOption = document.querySelector('input[name="level-options"]:checked');
-		// document.forms["difficultyLevelForm"]["level-options"].checked;
-	//} //end else-statement
 
 	return difficultyLevelRadioButtonOption;
-} //end difficultyLevelRadioButtonChecked() function
+} 
 
-//var difficultyLevelRadioButton = document.querySelector("input[name='level-options']:checked");
 
 /* Get the type of math buttons */
 var mathTypeButton = document.getElementsByClassName("mathType-button");
@@ -33,25 +23,15 @@ var additionButton = document.getElementById("addition-Button");
 var subtractionButton = document.getElementById("subtraction-Button");
 var multiplicationButton = document.getElementById("multiplication-Button");
 var divisionButton = document.getElementById("division-Button");
-
-/* Call the initializeGame() method when any of the math-Type buttons are clicked.  */
-/*for (var i = 0; i < mathTypeButton.length; i++) {
-	//Using an inline function, pass the node down to later get its value.
-	 //  Its value contains the operator for the type of math that is chosen. 
-
-	 var selectedButton = mathTypeButton[i];
-	mathTypeButton[i].addEventListener('click', function(event) {
-		console.log("Swag Value: " + selectedButton.value);
-		initializeGame(selectedButton);
-	} //end inline (click) function
-	); //end addEventListener 
-
-	//mathTypeButton[i].addEventListener("click", initializeGame(mathTypeButton[i]), false);
-} //end for loop */
+document.getElementById("highscore-Button").addEventListener("click", function() {
+	populateHighScores();
+	window.location.href = "/index.html#openModal";
+});
+document.getElementById("main-menu-button").addEventListener("click", function() {window.location.href = "/index.html";});
 
 additionButton.addEventListener('click', function(event) {
 		console.log("Swag Value: " + additionButton.value);
-		initializeGame(additionButton);
+        initializeGame(additionButton);
 	}); //end inline (click) function
 
 subtractionButton.addEventListener('click', function(event) {
@@ -82,59 +62,54 @@ function initializeGame(mathTypeSpecificButton) {
    prompting the user to do select the said options.  */
 function validateInputs(mathTypeSpecificButton) {
 	var mathTypeSwag = mathTypeSpecificButton;
-	if (checkPlayerName() == false) {
+	if (!checkPlayerName()) {
 		document.getElementById("nameInput-errorMessage").style.display = "inline-block";
-	}  //end if statement
-
-	if (checkPlayerName() == true) {
+	} else {
 		document.getElementById("nameInput-errorMessage").style.display = "none";
-	} //end if statement
+	} 
 
-	if (checkDifficultyLevel() == false) {
+	if (!checkDifficultyLevel()) {
 		console.log("Please choose a difficulty level");
 		document.getElementById("difficultyLevel-errorMessage").style.display = "inline-block";
-	} //end if-statement
-
-	if (checkDifficultyLevel() == true) {
+	} else {
 		document.getElementById("difficultyLevel-errorMessage").style.display = "none";
-	} //end if-statement
+	} 
 
-	if (checkPlayerName() == true && checkDifficultyLevel() == true) {
+	if (checkPlayerName() && checkDifficultyLevel()) {
 		console.log("Change to gameRun");
 		var userInfo = {}; 
 		userInfo.userName = playerName.value;
 		userInfo.difficultyLevel =  document.querySelector('input[name="level-options"]:checked').value;
-		//difficultyLevelRadioButton_Checked.value;
 		userInfo.mathType = mathTypeSwag.value;
+		userInfo.time = -1;
 
-		localStorage.setItem("userInfo", JSON.stringify(userInfo) );
+		localStorage.setItem("userInfo", JSON.stringify(userInfo));
+		console.log("cookie = " + JSON.stringify(userInfo));
 		window.location.pathname = "/gameRun.html";
 
 		console.log("Math Value of button: " + mathTypeSwag.value);
 		console.log(typeof mathTypeSwag.value);
+	} 
 
-		//location.href.replace(location.origin,'/gameRun.html');
-	} //end if-statement
-
-} //end validateInputs() function
+} 
 
 function checkPlayerName() {
 	if (playerName.value == "" || playerName.value === null || 
 		playerName.value.length == 0 || isEmpty(playerName.value) == true ) {
 		return false;
-	} //end if-statement
+	} 
 	if (playerName.value !== "" || playerName.value !== null || 
 		playerName.value.length != 0 || isEmpty(playerName.value) == false ) {
 		return true;
-	} //end if-statement
-} //end checkPlayerName() function
+	} 
+}
 
 /* Checks to see if there is whitespace in an element
    like an input field. Returns true if the field is empty.
 */
 function isEmpty(str){
     return !str.replace(/^\s+/g, '').length; 
-} //end isEmpty() function
+} 
 
 
 /* Checks if a difficulty level has been selected or not. */
@@ -146,19 +121,57 @@ function checkDifficultyLevel() {
 	for (var i = 0; i < diffLevelFormLength; i++) {
 		if (diffLevelRadioButton[i].checked) {
 			levelChosen = diffLevelRadioButton[i].value;
-		} //end if-statement
-	} //end for loop
+		} 
+	} 
 
 	if (levelChosen == null) {
 		return false;
-	} //end if-statement
+	} 
 
 	else {
 		return true;
-	} //end else-statement
-} //end checkDifficultyLevel() option
+	} 
+} 
 
 function returnDifficultyLevel() {
-	return difficultyLevelRadioButton_Checked.value;	
+	return difficultyLevelRadioButton_Checked.value;
+}
+function compare(user1, user2){
+    var data1 = user1.score;
+    var data2 = user2.score;
+    if (parseInt(data1) < parseInt(data2)) { return -1;}
+    if (parseInt(data1) > parseInt(data2)) { return 1;}
+    return 0;
+}
+function getTop10(){
+	if (objArray != null){
+		objArray.sort(compare);
+	}
 }
 
+function populateHighScores() {
+	getTop10();
+	if (objArray != null){
+		for (var i=0; i<objArray.length; i++){
+			if (i == 10) {return;}
+				if (objArray[i] != null){
+					var name = "user-name"+i;
+					var lvl = "user-lvl"+i;
+					var type = "user-type"+i;
+					var time = "user-time"+i;
+					document.getElementById(name).innerHTML = objArray[i].userName;
+					document.getElementById(lvl).innerHTML = objArray[i].difficultyLevel;
+       				var newMathType = convertMathType(objArray[i].mathType);
+					document.getElementById(type).innerHTML = newMathType;
+					document.getElementById(time).innerHTML = objArray[i].time;
+		}
+	}
+	}
+
+}
+function convertMathType(type){
+	if (type == "+") { return "Addition";}
+	else if (type == "-") { return "Subtraction";}
+	else if (type == "*") { return "Multiplication"; }
+	else { return "Division"; }
+}
